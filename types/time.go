c3bfae44ee87ed3cbdeefdfe0fcfcb0a8980d99f
YihuaChiu <mychiux413@gmail.com>
2025-07-04 12:03:47 +0800

    add Timestamp struct

--- types/time.go
+++ types/time.go
@@ -2,6 +2,9 @@ package t
 
 import (
 	"database/sql/driver"
+	"fmt"
+	"net/url"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -38,4 +41,62 @@ func (ts TimeArray) Value() (driver.Value, error) {
 	}
 	arr := "{" + strings.Join(strs, ",") + "}"
 	return arr, nil
+}
+
+// Ex: "1575856875" (seconds)
+type TimestampSec struct {
+	time.Time
+}
+
+func (t TimestampSec) EncodeValues(key string, v *url.Values) error {
+	j, err := t.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	v.Add(key, string(j))
+	return nil
+}
+
+func (dt *TimestampSec) UnmarshalJSON(bytes []byte) error {
+	i, err := strconv.ParseInt(string(bytes), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	dt.Time = time.Unix(i, 0)
+	return nil
+}
+
+func (dt *TimestampSec) MarshalJSON() ([]byte, error) {
+	str := fmt.Sprintf("%d", dt.Unix())
+	return []byte(str), nil
+}
+
+// Ex: "1575856875000" (milli seconds)
+type TimestampMs struct {
+	time.Time
+}
+
+func (t TimestampMs) EncodeValues(key string, v *url.Values) error {
+	j, err := t.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	v.Add(key, string(j))
+	return nil
+}
+
+func (dt *TimestampMs) UnmarshalJSON(bytes []byte) error {
+	i, err := strconv.Atoi(string(bytes))
+	if err != nil {
+		return err
+	}
+
+	dt.Time = time.UnixMilli(int64(i))
+	return nil
+}
+
+func (dt *TimestampMs) MarshalJSON() ([]byte, error) {
+	str := fmt.Sprintf("%d", dt.UnixMilli())
+	return []byte(str), nil
 }
